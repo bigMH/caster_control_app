342075e6411fd297adab70b3fd5332bba1aedae5
bigMH <928675689@qq.com>
2020-06-30 11:43:17 +0800

    commit

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -10,8 +10,14 @@ MainWindow::MainWindow(ros::NodeHandle& nh_, QWidget *parent) :
     if(!caster_client_.isServerConnected()) {
       ui->textEdit->setText("Server doesn't connect.");
     }
-    stents_ = nh_.advertise<std_msgs::Float64>("Something", 1);
-    pan_tilt_ = nh_.advertise<pan_tilt_msg::PanTiltCmd>("pan_tilt", 1);
+    stents_ = nh_.advertise<std_msgs::Float64>("/caster/body_controller/command", 1);
+    pan_tilt_ = nh_.advertise<pan_tilt_msg::PanTiltCmd>("/pan_tilt_driver_node/pan_tilt_cmd", 1);
+
+    stents_data_.data = 0.0;
+
+    pan_cmd_.speed = 0;
+    pan_cmd_.yaw   = 0.0;
+    pan_cmd_.pitch = 0.0;
 
     goal_.target_pose.pose.position.z = 0;
     goal_.target_pose.pose.orientation.x = 0;
@@ -36,6 +42,13 @@ int MainWindow::getCasterGoal() {
   return 0;
 }
 
+int MainWindow::getPanTiltCmd() {
+  pan_cmd_.yaw   = ui->lineEditYaw->text().toDouble();
+  pan_cmd_.pitch = ui->lineEditPitch->text().toDouble();
+  pan_cmd_.speed = ui->lineEditSpeed->text().toDouble();
+  return 0;
+}
+
 void MainWindow::on_pushButtonSendGoal_clicked(bool check) {
   getCasterGoal();
   
@@ -47,8 +60,22 @@ void MainWindow::on_pushButtonCancel_clicked(bool check) {
 
 }
 
+void MainWindow::on_pushButtonSendPanCmd_clicked(bool check) {
+  getPanTiltCmd();
+  pan_tilt_.publish(pan_cmd_);
+}
+
+void MainWindow::on_pushButtonSendHight_clicked(bool check) {
+  stents_data_.data = ui->lineEditHight->text().toDouble();
+  stents_.publish(stents_data_);
+}
+
 void MainWindow::on_pushButtonConnect_clicked(bool check) {
   ui->textEdit->setText("Waiting for server...");
   caster_client_.waitForServer();
   ui->textEdit->setText("Server has connected.");
+}
+
+void MainWindow::on_pushButtonExit_clicked(bool check) {
+  QApplication::exit();
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -3,6 +3,7 @@
 
 #include <ros/ros.h>
 #include <QMainWindow>
+#include <QApplication>
 // #include <ros/duration.h>
 // #include <std_msgs/String.h>
 #include <std_msgs/Float64.h>
@@ -24,20 +25,25 @@ public:
     explicit MainWindow(ros::NodeHandle& nh_, QWidget *parent = nullptr);
     ~MainWindow();
     int getCasterGoal();
+    int getPanTiltCmd();
  
 private:
     Ui::MainWindow *ui;
-    double stents_data_;
     ros::Publisher stents_;
     ros::Publisher pan_tilt_;
+    std_msgs::Float64 stents_data_;
     pan_tilt_msg::PanTiltCmd pan_cmd_;
     move_base_msgs::MoveBaseGoal goal_;
     actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> caster_client_;
 
 private slots:
-    void on_pushButtonSendGoal_clicked(bool check);
-    void on_pushButtonConnect_clicked(bool check);
+    void on_pushButtonExit_clicked(bool check);
     void on_pushButtonCancel_clicked(bool check);
+    void on_pushButtonConnect_clicked(bool check);
+    void on_pushButtonSendGoal_clicked(bool check);
+    void on_pushButtonSendHight_clicked(bool check);
+    void on_pushButtonSendPanCmd_clicked(bool check);
+
 };
 
 #endif // MAINWINDOW_H
